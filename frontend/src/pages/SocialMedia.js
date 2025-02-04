948c604748591acadbbc307a8b1c8faf4691db52
helia <helia.jm@gmail.com>
2025-02-04 01:18:54 +0100

    social media links

--- frontend/src/pages/SocialMedia.js
+++ frontend/src/pages/SocialMedia.js
@@ -1,72 +1,50 @@
 // File: src/pages/SocialMedia.js
 import React from "react";
-import { useContext } from "react";
-import { Routes, Route } from "react-router-dom";
-import { MobileViewContext } from "../context/MobileViewContext";
-import SecondarySidebarBasic from "../components/SecondarySidebarBasic";
+import { Routes, Route, useParams } from "react-router-dom";
 
 const socialItems = [
   {
     label: "Twitter",
     route: "/social-media/twitter",
-    logoUrl: "https://res.cloudinary.com/demo/image/upload/w_40,h_40/v1/twitter_logo.png",
+    logoUrl: "https://res.cloudinary.com/dl9xg597r/image/upload/v1738627019/5279123_tweet_twitter_twitter_logo_icon_whldcc.svg",
+    profileUrl: "https://x.com/JamshidiHelia",
   },
   {
     label: "LinkedIn",
     route: "/social-media/linkedin",
-    logoUrl: "https://res.cloudinary.com/demo/image/upload/w_40,h_40/v1/linkedin_logo.png",
+    logoUrl: "https://res.cloudinary.com/dl9xg597r/image/upload/v1738627019/5279114_linkedin_network_social_network_linkedin_logo_icon_wz4ntu.svg",
+    profileUrl: "https://www.linkedin.com/in/helia-jamshidi/",
   },
   {
     label: "GitHub",
     route: "/social-media/github",
-    logoUrl: "https://res.cloudinary.com/demo/image/upload/w_40,h_40/v1/github_logo.png",
+    logoUrl: "https://res.cloudinary.com/dl9xg597r/image/upload/v1738627019/394187_github_icon_izyihl.svg",
+    profileUrl: "https://github.com/helia94",
+  },
+  {
+    label: "Instagram",
+    route: "/social-media/instagram",
+    logoUrl: "https://res.cloudinary.com/dl9xg597r/image/upload/v1738627019/5279112_camera_instagram_social_media_instagram_logo_icon_eb5exo.svg",
+    profileUrl: "https://www.instagram.com/jouscards/",
   },
 ];
 
-function SocialHome() {
-  return (
-    <div>
-      <h2>Connect with Me</h2>
-      <p>Select a platform from the sidebar to visit my profile.</p>
-    </div>
-  );
-}
-
-function ExternalLink({ platform }) {
-  const platformUrls = {
-    twitter: "https://twitter.com/yourprofile",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    github: "https://github.com/yourprofile",
-  };
-
-  const url = platformUrls[platform];
-
-  return (
-    <div>
-      <h2>{platform.charAt(0).toUpperCase() + platform.slice(1)}</h2>
-      <button
-        className="external-visit-button"
-        onClick={() => window.open(url, "_blank")}
-      >
-        Visit {platform.charAt(0).toUpperCase() + platform.slice(1)}
-      </button>
-    </div>
-  );
-}
-
-export default function SocialMedia({isMobile}) {
-    const {contentIsVisibleMobile, toc2IsVisibleMobile } = useContext(MobileViewContext);
+export default function SocialMedia() {
   return (
     <div className="content-with-sub">
-      {(!isMobile || toc2IsVisibleMobile) && 
-      <SecondarySidebarBasic heading="Social Media" items={socialItems} />}
-      {(!isMobile || contentIsVisibleMobile) && 
-      <div className="content-area">
-        <Routes>
-          <Route path="/" element={<SocialHome />} />
-          <Route path=":platform" element={<ExternalLink />} />
-        </Routes>
-      </div>}
+      <div className="sub-sidebar">
+        <h3>Social Media</h3>
+        <ul className="simple-submenu" style={{ margin: "0", padding: "0"}} >
+          {socialItems.map((it) => (
+            <li key={it.label}>
+              <a className="submenu-item" style={{ display: "inline-flex", alignItems: "center"}} href={it.profileUrl}>
+                <img src={it.logoUrl} alt={`${it.label} logo`} className="submenu-logo" />
+                <span>{it.label}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
